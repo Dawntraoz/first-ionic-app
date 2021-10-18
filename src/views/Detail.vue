8a9dd996bbdaf7f5a83f03eee5291c9607eef564
Dawntraoz <albasilventefu@gmail.com>
2021-10-18 19:10:17 +0200

    feat: App creating and add few UI components to try it

---
+++ src/views/Detail.vue
@@ -0,0 +1,48 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Detail</ion-title>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/"></ion-back-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Detail</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <p>The dynamic ID is {{ id }}</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/vue';
+import { useRoute } from 'vue-router';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Detail',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonButtons, IonBackButton,
+  },
+  setup() {
+    const route = useRoute()
+    const id = route.params.id
+    return { id }
+  }
+});
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Blank</ion-title>
+        <ion-buttons slot="end">
+          <ion-button>
+            <ion-icon slot="icon-only" :icon="add"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <section>
+        <strong>Ready to create an app?</strong>
+        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      </section>
+
+      <ion-item v-for="item in items" :key="item.id" :router-link="`/detail/${item.id}`">
+        {{ item.name }}
+        <p slot="end">{{ item.id }}</p>
+      </ion-item>
+
+      <ion-button expand="full">Click me</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonItem } from '@ionic/vue';
+import { add } from 'ionicons/icons';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonItem,
+    IonIcon,
+  },
+  setup(){
+    const items = ref([{ name: 'foo', id: 1 }])
+    return { items, add }
+  }
+});
+</script>
+
+<style scoped>
+</style>
